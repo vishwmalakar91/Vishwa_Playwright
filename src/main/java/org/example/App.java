8bebc33d2cc19e1ec359f7cbbe564e12945ec321
Vishwanath Malakar <vishwanathmalakar@gmail.com>
2024-07-26 02:00:17 +0530

    with test ng

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,28 +9,32 @@ import java.nio.file.Paths;
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
+    public static void testPlaywright() {
         try (Playwright playwright = Playwright.create()) {
             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
             Page page = browser.newPage();
             page.navigate("http://playwright.dev");
-            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
+            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("outputs/screenshots/example.png")));
             System.out.println(page.title());
             browser.close();
 
             Browser ff_browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
             page = ff_browser.newPage();
             page.navigate("http://playwright.dev");
-            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example1.png")));
+            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("outputs/screenshots/example1.png")));
             System.out.println(page.title());
             browser.close();
 
             Browser safbrowser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false));
             page = safbrowser.newPage();
             page.navigate("http://playwright.dev");
-            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example3.png")));
+            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("outputs/screenshots/example3.png")));
             System.out.println(page.title());
             browser.close();
         }
+    }
+
+    public static void main(String[] args) {
+        testPlaywright();
     }
 }
